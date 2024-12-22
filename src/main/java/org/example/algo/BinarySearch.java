b2f24a0db13cc7d0885ab102a7a2046ed4bb1a7d
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-12-22 13:31:46 +0300

    binary search addedd

--- src/main/java/org/example/algo/BinarySearch.java
+++ src/main/java/org/example/algo/BinarySearch.java
@@ -2,6 +2,22 @@ package org.example.algo;
 
 public class BinarySearch {
 
+    public static int binarySearch(int[] nums, int target) {
+        int l = 0, u = nums.length - 1, m = nums.length / 2;
+
+        while(u >= l) {
+            m = l + (u-l) / 2; // (u-l) prevents integer overflow
+            if(target > nums[m]) {
+                l = m + 1;
+            } else if(target < nums[m]) {
+                u = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
     public static int searchInRotatedSortedArray(int[] nums, int target) {
 
         int lo = 0, hi = nums.length-1;
